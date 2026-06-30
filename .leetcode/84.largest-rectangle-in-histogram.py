cb1fda9d59357f80c671a01c4c235d2507a53b25
Lokesh Tiwari <lokeshtiwari001vns@gmail.com>
2026-06-30 17:36:07 +0000

    30 june 2026

--- .leetcode/84.largest-rectangle-in-histogram.py
+++ .leetcode/84.largest-rectangle-in-histogram.py
@@ -7,41 +7,31 @@
 # @lc code=start
 class Solution:
     """
-    time: 30 mins, with no ides to improve from brute
-    hint: studied the solution
-    time: O(4n) time, O(3n) space
+    time: 30 mins
+    hint: hint about the best solution
+    time: O(n) time, O(n) space
     pattern: monotonic stack
-    note: "next up learn on pass best solution"
     """
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-        widths = [1] * n
-        left = [] # left monotonic stack
-        right = [] # right monotonic stack
-        for i in range(n):
-            current_h = heights[i]
-            left_bound = -1
-            while left:
-                if heights[left[-1]] < current_h:
-                    left_bound = left[-1]
+        res = 0 
+        stack = [] # monotonic increasing stack
+        for idx, height in enumerate(heights):
+            while stack:
+                if heights[stack[-1]] < height:
                     break
-                left.pop()
-            left.append(i)
-            widths[i] += i - left_bound - 1
-
-            current_h = heights[n-i-1]
-            right_bound = n
-            while right:
-                if heights[right[-1]] < current_h:
-                    right_bound = right[-1]
-                    break
-                right.pop()
-            right.append(n-i-1)
-            widths[n-i-1] += right_bound - (n-i-1) - 1
-
-        res = 0
-        for i in range(n):
-            res = max(res, heights[i] * widths[i])
+                else:
+                    h = heights[stack.pop()]
+                    if stack:
+                        res = max(res, h * (idx - stack[-1] - 1))
+                    else:
+                        res = max(res, h * idx)
+            stack.append(idx)
+        
+        while stack:
+            h = heights[stack.pop()]
+            if stack:
+                res = max(res, h * (idx - stack[-1]))
+            else:
+                res = max(res, h * (idx+1))
         return res
-
 # @lc code=end
